0911decbfe5344433640d982a193cddd71d63bce
Patson T Lawrence <patsont90@gmail.com>
2025-08-05 21:53:53 +0300

    update

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 <Head>
+    <link rel="icon" href="/icon-192.ico" />
   <link rel="manifest" href="/manifest.json" />
   <meta name="theme-color" content="#0d6efd" />
   <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
