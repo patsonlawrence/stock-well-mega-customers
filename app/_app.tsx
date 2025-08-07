bfa9a254ad1daa4375538a8bf34d56ba912ffbaa
Patson T Lawrence <patsont90@gmail.com>
2025-08-07 19:50:37 +0300

    update

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,8 +1,20 @@
-import Head from 'next/head';
+// pages/_app.tsx
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-<Head>
-    <link rel="icon" href="/favicon.ico" />
-  <link rel="manifest" href="/manifest.json" />
-  <meta name="theme-color" content="#0d6efd" />
-  <link rel="apple-touch-icon" href="/public/favicon.ico" />
-</Head>
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0d6efd" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        {/* Optional: add different sizes/icons for better cross-device support */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
