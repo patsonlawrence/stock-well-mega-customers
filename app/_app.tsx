0a37dddc8296216b915d824bae430583e1ce57a6
Patson T Lawrence <patsont90@gmail.com>
2025-08-07 19:30:31 +0300

    update

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 
 <Head>
-    <link rel="icon" href="/icon-192.ico" />
+    <link rel="icon" href="/favicon.ico" />
   <link rel="manifest" href="/manifest.json" />
   <meta name="theme-color" content="#0d6efd" />
-  <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+  <link rel="apple-touch-icon" href="/public/favicon.ico" />
 </Head>
