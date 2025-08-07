f41bf8a4a6561653ee467dc80b5a52f76e910543
Patson T Lawrence <patsont90@gmail.com>
2025-08-07 20:15:46 +0300

    update

--- app/_app.tsx
+++ app/_app.tsx
@@ -6,10 +6,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/faviconv1.ico" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0d6efd" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/faviconv1.ico" />
         {/* Optional: add different sizes/icons for better cross-device support */}
       </Head>
       <Component {...pageProps} />
