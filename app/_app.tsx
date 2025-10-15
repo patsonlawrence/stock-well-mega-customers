746dfecd8f132c03f5534df38c748a018152a9f7
Patson T Lawrence <patsont90@gmail.com>
2025-10-15 18:40:38 +0300

    update

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,8 +1,20 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker
+          .register('/sw.js')
+          .then((reg) => console.log('Service Worker registered:', reg))
+          .catch((err) => console.error('Service Worker registration failed:', err))
+      })
+    }
+  }, [])
+
   return (
     <>
       <Head>
@@ -10,7 +22,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0d6efd" />
         <link rel="apple-touch-icon" href="/faviconv1.ico" />
-        {/* Optional: add different sizes/icons for better cross-device support */}
       </Head>
       <Component {...pageProps} />
     </>
